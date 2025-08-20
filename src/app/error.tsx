10141f5731f57474fa17fd05e505bbe634919f45
licoshome <licoshome2@gmail.com>
2025-08-20 17:41:37 +0900

    17:40

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -73,7 +73,7 @@ export default function Error({
           </button>
           
           <div className="flex justify-center">
-            <a
+            <link
               href="/"
               className="text-slate-400 hover:text-white transition-colors inline-flex items-center space-x-2"
             >
@@ -86,7 +86,7 @@ export default function Error({
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
               </svg>
               <span>홈으로 돌아가기</span>
-            </a>
+            </link>
           </div>
         </div>
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -33,7 +34,7 @@ export default function RootLayout({
                 />
             </head>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased` }
             >     
                 <GameRankHeader />
                 <HeaderBarHero  />
