f42768160bdacc29bec1f64272dd01e5015390e1
licoshome <licoshome2@gmail.com>
2025-09-01 14:24:59 +0900

    mm

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,49 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com" , "cdn.gamemeca.com"],
+    domains: [
+      "images.unsplash.com",
+      "cdn.gamemeca.com",
+      "shared.akamai.steamstatic.com",
+      "cdn.akamai.steamstatic.com",
+      "steamcdn-a.akamaihd.net",
+      "steamcdn-b.akamaihd.net",
+      "steamstatic.com",
+    ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "shared.akamai.steamstatic.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.akamai.steamstatic.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "steamcdn-a.akamaihd.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "steamcdn-b.akamaihd.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "steamstatic.com",
         port: "",
         pathname: "/**",
       },
